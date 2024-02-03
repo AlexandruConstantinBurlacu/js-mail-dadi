0bf5f0bb9d7ce1902c0fd693547f596727e40591
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-03 11:48:56 +0100

    inserimento array mail casuali

---
+++ Mail/script.js
@@ -0,0 +1,26 @@
+// array per le mail
+
+const mailArray = [
+    'john.doe@email123.com',
+    'alice.smith@email456.com',
+    'sam.jackson@email789.com',
+    'emma.white@emailabc.com',
+    'robert.green@emailxyz.com',
+    'lisa.miller@email987.com',
+    'brian.jones@email321.com',
+    'olivia.black@emaildef.com',
+    'kevin.wilson@email654.com',
+    'mia.brown@emailghi.com',
+    'chris.taylor@email000.com',
+    'sophia.evans@email999.com',
+    'daniel.morris@email888.com',
+    'ava.clark@email777.com',
+    'michael.young@email666.com',
+    'grace.walker@email555.com',
+    'jason.martin@email444.com',
+    'emily.hill@email333.com',
+    'jacob.kelly@email222.com',
+    'isabella.cooper@email111.com',
+]
+
+console.log(mailArray);
