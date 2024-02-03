aa0f1539656adcd94d6f72d21c6638e334927143
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-03 11:06:19 +0100

    inserimento condizioni vittoria, sconfitta, pareggio

--- Gioco Dei Dadi/script.js
+++ Gioco Dei Dadi/script.js
@@ -16,3 +16,13 @@ document.getElementById("roll-dice").onclick = function() {
     document.getElementById("player-dice").innerHTML = casualPlayer;
     document.getElementById("computer-dice").innerHTML = casualComputer;
 }
+
+// aggiungo le condizioni vittoria, sconfitta, pareggio
+
+if (casualPlayer > casualComputer) {
+    document.getElementById("stats").innerHTML = "You are lucky today";
+} else if (casualPlayer < casualComputer) {
+    document.getElementById("stats").innerHTML = "Not the best day today, huh?";
+} else {
+    document.getElementById("stats").innerHTML = "Nice but not good";
+}
